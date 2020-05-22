82db9a18dedd73b099904bf2d0e1b4e66a44b693
coco369444 <49324047+coco369444@users.noreply.github.com>
2020-05-22 16:22:34 +0200

    Update plotsurface.py

--- volatility_surface/plotsurface.py
+++ volatility_surface/plotsurface.py
@@ -11,17 +11,17 @@ import numpy as np
 #import seaborn as sns
 import plotly.graph_objects as go
 
-
+#We compute the 3D surface with the right volatility matrix, maturity and strike vectors
 def affichage3D(sigma,maturity,strike, _plot = True):
     
-    fig = go.Figure(data=[go.Surface(x=maturity,
+    fig = go.Figure(data=[go.Surface(x=maturity, #we compute the figure
                                      y=strike,
                                      z=sigma,
                                      colorscale='RdBu',
                                 
                                      colorbar = {'lenmode': 'fraction', 'len': 0.70},
                                      
-                                     hovertemplate = 'Maturity: %{x}'+
+                                     hovertemplate = 'Maturity: %{x}'+ #we compute the coordinates names
                                      '<br>Strike: %{y:.0f}<br>'+
                                      'Volatility: %{z:.0f}<extra></extra>',
                                      
@@ -62,7 +62,7 @@ def affichage3D(sigma,maturity,strike, _plot = True):
                              zerolinecolor = '#292724'),
                 ),
             
-            font = dict(color = '#f5f5f5'),
+            font = dict(color = '#f5f5f5'), #we compute the different color of the graphic
 
             margin=dict(r=0, b=0, l=0, t=0),
             
@@ -81,48 +81,49 @@ def affichage3D(sigma,maturity,strike, _plot = True):
     
     return fig
 
+#we full the empty intervals to smooth the surface
 def remplacenull(liste_sig,list_null,strike_fin):
     
     n,m = np.shape(list_null)
-    for j in range(m):
+    for j in range(m): #for each columns (maturity)
         list_nullj = list_null[:,j]
-        list_nullm= []
-        for k in range(len(list_nullj)):
+        list_nullm= [] #we search the empty data
+        for k in range(len(list_nullj)): #we compute a new list where we get the coordinate of the empty data
             if list_nullj[k]==1:
                 list_nullm.append(k)
         exit=0
         ind0=0
         
-        while exit==0 and len(list_nullm)>0:
+        while exit==0 and len(list_nullm)>0: #we replace every empty data
             dep=ind0 +0
-            listi=[list_nullm[ind0]]
+            listi=[list_nullm[ind0]] #we set the first empty data
     
-            while ind0+1<len(list_nullm) and list_nullm[ind0+1] == list_nullm[ind0]+1 :
+            while ind0+1<len(list_nullm) and list_nullm[ind0+1] == list_nullm[ind0]+1 : #we take the consecutif empty intervals
                 
                 listi.append(list_nullm[ind0+1])
                 ind0=ind0+1
             
-            if len(listi) > 0:
-                for i in range(len(listi)):
-                    
+            if len(listi) > 0: 
+                for i in range(len(listi)): #we feel the data with a linear regreassion
+                    #between interval before first empty interval and the one after the last consecutive empty interval
                     h= (liste_sig[min(list_nullm[ind0]+1,n-1),j]- liste_sig[max(0,list_nullm[dep]-1),j])/(strike_fin[min(list_nullm[ind0]+1,n-1)] -strike_fin[max(0,list_nullm[dep]-1)] )
                     liste_sig[i+list_nullm[dep],j]= liste_sig[max(0,list_nullm[dep]-1),j] + (strike_fin[i+list_nullm[dep]]- strike_fin[max(0,list_nullm[dep]-1)])*h
             
-            ind0=ind0+1
+            ind0=ind0+1 #we pass to the next empty value
             if ind0 >= len(list_nullm):
                 exit=1
     return liste_sig
     
     
-
+#We create the different strike intervals and fill it
 def step_std_from_strike(std,liste_strike,strike,h):
     
     N = len(liste_strike)
     liste_sig= np.zeros(N)
     list_null=[]
-    for i in range(N):
+    for i in range(N): #for each strike point we take the volatility where the strike is in a strike interval
         mask1= (strike < liste_strike[i]+h) & (strike >= liste_strike[i]-h)
-        if len(std[mask1])== 0:
+        if len(std[mask1])== 0:# if we do not find any data we save this information is a list
             list_null.append(i)
         else :
             liste_sig[i]=np.mean(std[mask1])
@@ -131,20 +132,20 @@ def step_std_from_strike(std,liste_strike,strike,h):
         
     return liste_sig,list_null
     
-
+#We compute the 3D surface from the data calculated with the Black-Scholes Merton model 
 def aff_surface(sigma_calcul,strike,maturity,_plot=True):
     
     maturity_surf= np.sort(np.unique(maturity))
     strike_u = np.sort(np.unique(strike))
-    strike_surf= np.linspace(min(strike_u),max(strike_u)+0.1,int(len(strike_u)/2))
-    h = (strike_surf[-1]-strike_surf[0])/len(strike_surf)
-    sig_surf=np.zeros((len(strike_surf),len(maturity_surf)))
-    list_null=np.zeros((len(strike_surf),len(maturity_surf)))
-    for m in range(len(maturity_surf)):
-        mask = maturity==maturity_surf[m]
+    strike_surf= np.linspace(min(strike_u),max(strike_u)+0.1,int(len(strike_u)/2)) # We compute the different normalize grid
+    h = (strike_surf[-1]-strike_surf[0])/len(strike_surf) # we compute the lenght of each intervals
+    sig_surf=np.zeros((len(strike_surf),len(maturity_surf))) #we create the final volatility surface
+    list_null=np.zeros((len(strike_surf),len(maturity_surf)))#we compute a array where we can know where is the empty observations
+    for m in range(len(maturity_surf)): #For each different maturity we compute the average volatility per strike intervals
+        mask = maturity==maturity_surf[m] 
         
         
-        sigm,list_nulli = step_std_from_strike(sigma_calcul[mask],strike_surf,strike[mask],h)
+        sigm,list_nulli = step_std_from_strike(sigma_calcul[mask],strike_surf,strike[mask],h) #we compute the volatility for this maturity
         for j in list_nulli:
             list_null[j,m]=1
         sig_surf[:,m]=sigm
@@ -152,7 +153,7 @@ def aff_surface(sigma_calcul,strike,maturity,_plot=True):
     sig_surf=remplacenull(sig_surf,list_null,strike_surf)
     maturity_surf2= np.empty(len(maturity_surf),dtype=object)
     
-    for i in range(len(maturity_surf)):
+    for i in range(len(maturity_surf)): #we compute the maturity coordinate name, to have a understandable information
         
         if maturity_surf[i]*12 <1:
             maturity_surf2[i] =str(int((maturity_surf[i]*12-int(maturity_surf[i]*12))*30) ) + 'D'
@@ -172,10 +173,5 @@ def aff_surface(sigma_calcul,strike,maturity,_plot=True):
     
     return fig 
 
-#test 
-#std=np.array([0,1,2,5,2,6,8,9])
-#strike=np.array([10,12,20,22,80,60,25,50])
-#mat=np.array([1,1,3,3,1,2,2,3])
 
-#aff_surface(std,strike,mat)
 
